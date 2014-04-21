8e1ec1b96d8adcf46fa2b0270e17f51285928ed5
Robert Fryman <robert.fryman@gmail.com>
2014-04-21 10:32:56 -0500

    added to_s method on month class to be able to print months

--- test/test_cal_unit.rb
+++ test/test_cal_unit.rb
@@ -257,5 +257,18 @@ class LinkedListTest < MiniTest::Unit::TestCase
     assert_equal("Monday", cal.start_day)
   end
 
+  def test_11_to_s
+    cal = Month.new(1,2014)
+    assert_equal("    January 2014    
+Su Mo Tu We Th Fr Sa
+          1  2  3  4
+ 5  6  7  8  9 10 11
+12 13 14 15 16 17 18
+19 20 21 22 23 24 25
+26 27 28 29 30 31   
+                    
+", cal.to_s)
+  end
+
 end
 
