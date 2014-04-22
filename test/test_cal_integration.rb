26dd52f81535b294804cb8279aa798311825d8b9
Robert Fryman <robert.fryman@gmail.com>
2014-04-22 14:59:26 -0500

    added whitespace_eliminator method to allow intergration testing for year

--- test/test_cal_integration.rb
+++ test/test_cal_integration.rb
@@ -2,6 +2,14 @@ require 'nss'
 require 'zeller'
 require 'month'
 
+def unix_whitespace_eliminator lines
+  output = ""
+  lines.each_line do |line|
+    output.concat(line.rstrip + "\n")
+  end
+  return output
+end
+
 class CalIntergrationTest < Test::Unit::TestCase
   def test_12a_cal_Intergration_month
     assert_equal(`cal 01 1800`, `./cal 1 1800`)
@@ -27,8 +35,20 @@ class CalIntergrationTest < Test::Unit::TestCase
   def test_12h_cal_Intergration_month
     assert_equal(`cal 03 2015`, `./cal 3 2015`)
   end
-  def test_13_cal_intergration_year
-    skip
-    assert_equal(`cal 2013`, `./cal 2013`)
+  def test_13a_cal_intergration_year
+    expected_output = unix_whitespace_eliminator `cal 2013`
+    assert_equal(expected_output, `./cal 2013`)
+  end
+  def test_13b_cal_intergration_year
+    expected_output = unix_whitespace_eliminator `cal 2012`
+    assert_equal(expected_output, `./cal 2012`)
+  end
+  def test_13c_cal_intergration_year
+    expected_output = unix_whitespace_eliminator `cal 2000`
+    assert_equal(expected_output, `./cal 2000`)
+  end
+  def test_13d_cal_intergration_year
+    expected_output = unix_whitespace_eliminator `cal 1990`
+    assert_equal(expected_output, `./cal 1990`)
   end
 end
