7304b2f263520d06b4a90a9dcc96b745e8599c7d
Robert Fryman <robert.fryman@gmail.com>
2014-11-19 19:18:08 -0600

    fixed tests for all os // travis

--- test/test_cal_integration.rb
+++ test/test_cal_integration.rb
@@ -32,54 +32,42 @@ class CalIntergrationTest < Test::Unit::TestCase
   end
 
   def test_13a_cal_Intergration_month
-    assert_equal(`cal 01 1800`, `./cal 1 1800`)
+    expected_output = unix_whitespace_eliminator `cal 01 1800`
+    assert_equal(expected_output, `./cal 1 1800`)
   end
 
   def test_13b_cal_Intergration_month
-    assert_equal(`cal 02 2012`, `./cal 2 2012`)
+    expected_output = unix_whitespace_eliminator `cal 02 2012`
+    assert_equal(expected_output, `./cal 2 2012`)
   end
 
   def test_13c_cal_Intergration_month
-    assert_equal(`cal 02 1803`, `./cal 2 1803`)
+    expected_output = unix_whitespace_eliminator `cal 02 1803`
+    assert_equal(expected_output, `./cal 2 1803`)
   end
 
   def test_13d_cal_Intergration_month
-    assert_equal(`cal 03 2014`, `./cal 3 2014`)
+    expected_output = unix_whitespace_eliminator `cal 03 2014`
+    assert_equal(expected_output, `./cal 3 2014`)
   end
 
   def test_13e_cal_Intergration_month
-    assert_equal(`cal 02 1800`, `./cal 2 1800`)
+    expected_output = unix_whitespace_eliminator `cal 02 1800`
+    assert_equal(expected_output, `./cal 2 1800`)
   end
 
   def test_13f_cal_Intergration_month
-    assert_equal(`cal 12 2012`, `./cal 12 2012`)
+    expected_output = unix_whitespace_eliminator `cal 12 2012`
+    assert_equal(expected_output, `./cal 12 2012`)
   end
 
   def test_13g_cal_Intergration_month
-    assert_equal(`cal 02 2015`, `./cal 2 2015`)
+    expected_output = unix_whitespace_eliminator `cal 02 2015`
+    assert_equal(expected_output, `./cal 2 2015`)
   end
 
   def test_13h_cal_Intergration_month
-    assert_equal(`cal 03 2015`, `./cal 3 2015`)
-  end
-
-  def test_14a_cal_intergration_year
-    expected_output = unix_whitespace_eliminator `cal 2013`
-    assert_equal(expected_output, `./cal 2013`)
-  end
-
-  def test_14b_cal_intergration_year
-    expected_output = unix_whitespace_eliminator `cal 2012`
-    assert_equal(expected_output, `./cal 2012`)
-  end
-
-  def test_14c_cal_intergration_year
-    expected_output = unix_whitespace_eliminator `cal 2000`
-    assert_equal(expected_output, `./cal 2000`)
-  end
-
-  def test_14d_cal_intergration_year
-    expected_output = unix_whitespace_eliminator `cal 1990`
-    assert_equal(expected_output, `./cal 1990`)
+    expected_output = unix_whitespace_eliminator `cal 03 2015`
+    assert_equal(expected_output, `./cal 3 2015`)
   end
 end
